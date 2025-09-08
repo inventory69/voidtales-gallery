25f765847c2db02ce2e9c48f005e446c4b1c75ac
inventory69 <admin@sad.run>
2025-09-08 08:46:19 +0200

    Refactor build process: remove gen:thumbs-ext script and add copy-original-images script for downloading images

---
+++ scripts/copy-original-images.cjs
@@ -0,0 +1,31 @@
+const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const destDir = './public/images/original/';
+const webserverUrl = 'http://172.19.0.1:8723/original/'; // interne IP und Pfad zum Ordner
+
+(async () => {
+  // Hole das Directory Listing als HTML
+  const res = await axios.get(webserverUrl);
+  const $ = cheerio.load(res.data);
+
+  // Extrahiere alle Links auf .md-Dateien
+  const files = [];
+  $('a').each((_, el) => {
+    const href = $(el).attr('href');
+    if (href && href.endsWith('.md')) {
+      files.push(href);
+    }
+  });
+
+  // Lade jede Datei per wget herunter
+  files.forEach(file => {
+    const url = `${webserverUrl}${file}`;
+    const destPath = path.join(destDir, file);
+    console.log(`Downloading ${url} -> ${destPath}`);
+    execSync(`wget -q -O "${destPath}" "${url}"`);
+  });
+})();
