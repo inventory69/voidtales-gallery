4383662891c0f18a32924c0e9cbbbb4174344196
inventory69 <admin@sad.run>
2025-09-08 11:31:36 +0200

    Remove default URLs from external download config and clean up Fancybox styles

--- src/config/externaldownload.cjs
+++ src/config/externaldownload.cjs
@@ -1,8 +1,8 @@
 module.exports = {
   enableCopyMdFiles: true,
   enableCopyOriginalImages: true,
-  mdSourceUrlInternal: process.env.EXT_DL_URL_MARKDOWN || 'http://172.19.0.1:8723/markdown/',
-  mdSourceUrlExternal: process.env.EXT_DL_URL_MARKDOWN_EXTERNAL || 'http://168.119.127.193:8723/markdown/',
-  originalSourceUrlInternal: process.env.EXT_DL_URL_ORIGINAL || 'http://172.19.0.1:8723/original/',
-  originalSourceUrlExternal: process.env.EXT_DL_URL_ORIGINAL_EXTERNAL || 'http://168.119.127.193:8723/original/',
+  mdSourceUrlInternal: process.env.EXT_DL_URL_MARKDOWN,
+  mdSourceUrlExternal: process.env.EXT_DL_URL_MARKDOWN_EXTERNAL,
+  originalSourceUrlInternal: process.env.EXT_DL_URL_ORIGINAL,
+  originalSourceUrlExternal: process.env.EXT_DL_URL_ORIGINAL_EXTERNAL,
 };
